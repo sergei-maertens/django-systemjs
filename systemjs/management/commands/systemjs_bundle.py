10d88e8d061ed86cebc6436e9a051d8dfc683cdc
Sergei Maertens <sergeimaertens@gmail.com>
2016-04-27 00:17:38 +0200

    Support bundling apps in specific templates
    Fixes #11

--- systemjs/management/commands/systemjs_bundle.py
+++ systemjs/management/commands/systemjs_bundle.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-import argparse
 import io
 import os
 import re
@@ -13,14 +12,18 @@ from django.core.management.base import BaseCommand
 from django.core.management.utils import handle_extensions
 from django.core.files.storage import FileSystemStorage
 from django.template.base import TOKEN_BLOCK
+from django.template import loader
 from django.template.loaders.app_directories import get_app_template_dirs
 
 from systemjs.base import System
 from systemjs.compat import Lexer
 from systemjs.jspm import find_systemjs_location
+from systemjs.templatetags.system_tags import SystemImportNode
 
 
 SYSTEMJS_TAG_RE = re.compile(r"""systemjs_import\s+(['\"])(?P<app>.*)\1""")
+
+RESOLVE_CONTEXT = {}
 
 
 class Command(BaseCommand):
@@ -42,15 +45,22 @@ class Command(BaseCommand):
 
         parser.add_argument('--minify', action='store_true', help='Let jspm minify the bundle')
 
-        parser.add_argument(
+        tpl_group = parser.add_mutually_exclusive_group()
+        tpl_group.add_argument(
             '--extension', '-e', dest='extensions',
             help='The file extension(s) to examine (default: "html"). Separate '
                  'multiple extensions with commas, or use -e multiple times.',
             action='append')
+        tpl_group.add_argument(
+            '--template', '-t', dest='templates',
+            help='The templates to examine. Separate multiple template names with'
+                 'commas, or use -t multiple times',
+            action='append')
+
         parser.add_argument(
             '--symlinks', '-s', action='store_true', dest='symlinks',
             default=False, help='Follows symlinks to directories when examining '
-                                'source code and templates for translation strings.')
+                                'source code and templates for SystemJS imports.')
         parser.add_argument(
             '--no-post-process',
             action='store_false', dest='post_process', default=True,
@@ -75,27 +85,40 @@ class Command(BaseCommand):
             template_dirs += list(config['DIRS'])
 
         # find all template files
-
-        all_files = []
-        for template_dir in template_dirs:
-            for dirpath, dirnames, filenames in os.walk(template_dir, topdown=True, followlinks=self.symlinks):
-                for filename in filenames:
-                    filepath = os.path.join(dirpath, filename)
-                    file_ext = os.path.splitext(filename)[1]
-                    if file_ext not in self.extensions:
-                        continue
-                    all_files.append(filepath)
-
         all_apps = []
-        for fp in all_files:
-            with io.open(fp, 'r', encoding=settings.FILE_CHARSET) as template_file:
-                src_data = template_file.read()
+        if not options.get('templates'):
+            all_files = []
+            for template_dir in template_dirs:
+                for dirpath, dirnames, filenames in os.walk(template_dir, topdown=True, followlinks=self.symlinks):
+                    for filename in filenames:
+                        filepath = os.path.join(dirpath, filename)
+                        file_ext = os.path.splitext(filename)[1]
+                        if file_ext not in self.extensions:
+                            continue
+                        all_files.append(filepath)
 
-            for t in Lexer(src_data).tokenize():
-                if t.token_type == TOKEN_BLOCK:
-                    imatch = SYSTEMJS_TAG_RE.match(t.contents)
-                    if imatch:
-                        all_apps.append(imatch.group('app'))
+            for fp in all_files:
+                with io.open(fp, 'r', encoding=settings.FILE_CHARSET) as template_file:
+                    src_data = template_file.read()
+
+                for t in Lexer(src_data).tokenize():
+                    if t.token_type == TOKEN_BLOCK:
+                        imatch = SYSTEMJS_TAG_RE.match(t.contents)
+                        if imatch:
+                            all_apps.append(imatch.group('app'))
+        else:
+            for tpl in options.get('templates'):
+                template = loader.get_template(tpl)
+                import_nodes = template.template.nodelist.get_nodes_by_type(SystemImportNode)
+                for node in import_nodes:
+                    app = node.path.resolve(RESOLVE_CONTEXT)
+                    if not app:
+                        self.stdout.write(self.style.WARNING(
+                            '{tpl}: Could not resolve path with context {ctx}, skipping.'.format(
+                                tpl=tpl, ctx=RESOLVE_CONTEXT)
+                        ))
+                        continue
+                    all_apps.append(app)
 
         bundled_files = OrderedDict()
         # FIXME: this should be configurable, if people use S3BotoStorage for example, it needs to end up there
@@ -108,17 +131,17 @@ class Command(BaseCommand):
                 self.stdout.write('Bundled {app} into {out}'.format(app=app, out=rel_path))
             bundled_files[rel_path] = (storage, rel_path)
 
-        # post-process system.js if it's within settings.STATIC_ROOT
-        systemjs_path = find_systemjs_location()
-        try:
-            within_static_root = self.storage.exists(systemjs_path)
-        except SuspiciousFileOperation:
-            within_static_root = False
-        if within_static_root:
-            relative = os.path.relpath(systemjs_path, settings.STATIC_ROOT)
-            bundled_files[relative] = (storage, relative)
-
         if self.post_process and hasattr(self.storage, 'post_process'):
+            # post-process system.js if it's within settings.STATIC_ROOT
+            systemjs_path = find_systemjs_location()
+            try:
+                within_static_root = self.storage.exists(systemjs_path)
+            except SuspiciousFileOperation:
+                within_static_root = False
+            if within_static_root:
+                relative = os.path.relpath(systemjs_path, settings.STATIC_ROOT)
+                bundled_files[relative] = (storage, relative)
+
             processor = self.storage.post_process(bundled_files, dry_run=False)
             for original_path, processed_path, processed in processor:
                 if isinstance(processed, Exception):  # pragma: no cover
